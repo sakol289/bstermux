7844c11d49d368f5b38b069bf9b6bb703901a166
อาเจ <82790183+sakol289@users.noreply.github.com>
2021-12-08 08:17:27 -0500

    Add files via upload

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import os
 import sys
 import time
 import webbrowser
-#from playsound import playsound
+# from playsound import playsound
 
 
 # ฟังชั้้นต่างๆ
@@ -45,18 +45,25 @@ memu = ('''
             \33[32mM\33[33mE\33[35mM\33[37mU\33[35mS\33[32mY\33[36mS\33[33mT\33[32mE\33[35mM
 \33[32m [ 1 ] \33[33m INSTALL 
 \33[32m [ 2 ] \33[33m CR
+\33[32m [ 3 ] \33[33m UPDATE
 \33[32m [99 ] \33[33m EXIT
 
 ''')
+os.system("clear")
 lowprint(memu)
-#playsound('1.mp3')
+# playsound('1.mp3')
 x = int(input("ENTER NEMBER : "))
-if x == 1:
-    os.system("pkg install wget -y;apt install wget -y;cd $HOME;cd ..;cd usr;cd etc;rm -rf bash.bashrc;wget https://raw.githubusercontent.com/sakol289/bashterumxv1/main/bash.bashrc")
-if x == 3:
+if x == 1:#cr rj
+    os.system("clear")
+    lowprint(top)
+    os.system("pkg install wget -y;apt install wget -y;cd $HOME;cd ..;cd usr;cd etc;rm -rf bash.bashrc;wget https://raw.githubusercontent.com/sakol289/bashterumxv1/main/bash.bashrc;exit")#crrj
+if x == 2:
     os.system("clear")
     lowprint(cr)
     lowprint(xr)
     input("PLESS ENTER : ")
+if x == "3":
+    os.system("git pull https://github.com/sakol289/bstermux")
+    os.system("python3 run.py")
 if x == 99:
-    os.system("exit")
+    os.system("exit")#crrj
